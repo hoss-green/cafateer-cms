b67a227af267bd49036c0d1d80366112eb132bc2
Hoss <hoss.green@pm.me>
2024-06-27 11:00:29 +0100

    cleanup, udpdate and removed build files from repo

--- rust_proj/src/manager/templates/component_buttons.rs
+++ rust_proj/src/manager/templates/component_buttons.rs
@@ -1,18 +1,6 @@
 use super::view_models::DropDownLanguageVm;
-// use askama::Template;
-
-// #[derive(Template, Debug, Clone)]
-// #[template(path = "manager/macros/language_selection_dropdown.html")]
-// pub struct PrimaryLanguageButtonVm {
-//     pub id: uuid::Uuid,
-//     pub title: String,
-//     pub category: String,
-//     pub user_languages: Vec<DropDownLanguageVm>,
-//     pub published: bool,
-// }
 
 #[derive(Debug, Clone)]
-// #[template(path = "manager/macros/language_selection_dropdown.html")]
 pub struct MenuItemButtonVm {
     pub id: uuid::Uuid,
     pub title: String,
@@ -22,7 +10,6 @@ pub struct MenuItemButtonVm {
 }
 
 #[derive(Debug, Clone)]
-// #[template(path = "manager/macros/language_selection_dropdown.html")]
 pub struct CategoryButtonVm {
     pub id: uuid::Uuid,
     pub title: String,
